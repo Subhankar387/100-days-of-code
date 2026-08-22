0d30ce025a5c47a61e33a83a064c89e07fefd3f8
Subhankar Nag <Subhankar.40211@stu.upes.ac.in>
2026-08-22 17:49:49 +0530

    Add files via upload
    Day -1 Q1 Write a program to input two numbers and display their sum.

---
+++ sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int a,b;
+    printf("Enter the number a:");
+    scanf("%d",&a);
+    printf("Enter the number b:");
+    scanf("%d",&b);
+    int sum=a+b;
+    printf("The sum of a and b is %d\n",sum);
+    return 0;
+}
