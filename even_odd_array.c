b5f0ac5008381d1abb9f7b04ec9ef836c4107f46
Subhankar Nag <Subhankar.40211@stu.upes.ac.in>
2026-09-24 17:35:41 +0530

    Add files via upload
    Day 30 Q1 Count even and odd numbers in an array.

---
+++ even_odd_array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5, 6};
+    int n = 6;
+    int even = 0, odd = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] % 2 == 0)
+            even++;
+        else
+            odd++;
+    }
+
+    printf("Even numbers: %d\n", even);
+    printf("Odd numbers: %d\n", odd);
+
+    return 0;
+}
