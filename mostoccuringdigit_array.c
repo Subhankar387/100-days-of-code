9778ce5a52cc4b94a9bb7abf16d02f788b4c1b96
Subhankar Nag <Subhankar.40211@stu.upes.ac.in>
2026-09-26 12:20:24 +0530

    Add files via upload
    Day 32 Q2 Find the digit that occurs the most times in an integer number.

---
+++ mostoccuringdigit_array.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int n, digit, i;
+    int count[10] = {0};
+    int max = 0, maxDigit = 0;
+
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+
+    if (n < 0)
+        n = -n;
+
+    while (n > 0) {
+        digit = n % 10;
+        count[digit]++;
+        n = n / 10;
+    }
+
+    for (i = 0; i < 10; i++) {
+        if (count[i] > max) {
+            max = count[i];
+            maxDigit = i;
+        }
+    }
+
+    printf("Digit %d occurs the most times: %d times\n", maxDigit, max);
+
+    return 0;
+}
